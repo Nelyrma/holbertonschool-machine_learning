caa56bfcea4f38e2964b337e79cd8d18766a616a
marlyne <=>
2023-12-05 13:26:51 +0100

    shape of a matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 """the function calculates the shape of a matrix"""
 
+
 def matrix_shape(matrix):
+    """calculate the shape of a matrix
+
+    Args:
+        matrix: matrix for which to calcute the shape
+
+    Returns:
+        list of integers
+    """
     if not matrix or not isinstance(matrix, list):
         return []
 
