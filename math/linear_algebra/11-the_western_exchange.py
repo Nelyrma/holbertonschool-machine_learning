3202a5fe63b5acfce68c175daff4b976ab36e761
marlyne <=>
2023-12-08 14:44:40 +0100

    transpose a numpy.ndarray

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""function that transposes a matrix"""
+
+
+def np_transpose(matrix):
+    """transpose a matrix
+
+    Args:
+        matrix : the matrix to transpose
+
+    Returns:
+        a new matrix
+    """
+    return matrix.transpose()
