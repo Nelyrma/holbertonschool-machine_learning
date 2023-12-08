ddadf0ec967a760483152f8d603da7106f359585
marlyne <=>
2023-12-08 14:04:16 +0100

    function that perfoms matrix multiplication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -16,7 +16,8 @@ def mat_mul(mat1, mat2):
         return None
 
     # initialise the result matrix
-    result_matrix = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+    result_matrix = [[0 for _ in range(len(mat2[0]))]
+                     for _ in range(len(mat1))]
 
     # i : index column for mat1 
     for i in range(len(mat1)):
