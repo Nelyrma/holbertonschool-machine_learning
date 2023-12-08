9a431456a8f2300112c6580e555cf5b61902f84c
marlyne <=>
2023-12-08 14:36:18 +0100

    calculate the shape of a numpy.ndarray

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""function that calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """calculate the shape of a matrix
+
+    Args:
+        matrix : the matrix for which the shape is calculated
+
+    Returns:
+        _type_: _description_
+    """
+    return matrix.shape
