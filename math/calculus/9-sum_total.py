70e16e96bcd234fb92637f469974e140de44d42d
marlyne <=>
2023-12-13 11:23:40 +0100

    Task 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Function that calculates the summation"""
+
+
+def summation_i_squared(n):
+    """calculate the summmation
+
+    Args:
+        n (_int_): the stopping condition
+
+    Returns:
+        _int_: _an integer value of the sum_
+    """
+    # check if n is a valid integer
+    if not isinstance(n, int) and n <= 0:
+        return None
+    
+    # sum of square of n numbers
+    return [n * (n + 1) * (2 * n + 1)] / 6
